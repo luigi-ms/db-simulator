a213f3f2dcc1df292b977d4b33ff65dd57f40121
Luigi Moraes <luigimoraessantos@gmail.com>
2023-05-19 15:19:21 -0300

    finishing add functionality

--- script.js
+++ script.js
@@ -13,25 +13,48 @@ createApp({
         name: '',
         age: 0
       },
-      records: [
-        {
-          id: 0,
-          name: 'admin',
-          age: 99
-        }
-      ]
+      records: []
     };
   },
   methods: {
     addData(){
-      if(!this.idExists(this.entryForm.id)){
-        this.records.push(this.workingRecord);
+      this.records.push(this.mountWorkingRecord());
+    },
+    updateData(){
+      //mount working record and update record in records array
+    },
+    removeData(){},
+    mountWorkingRecord(){
+      try{
+        const col = this.validateColumn(this.entryForm.column);
+
+        this.workingRecord.id = this.validateID(this.entryForm.id);
+
+        if(col === 'name'){
+          this.workingRecord.name = this.entryForm.data;
+        }else if(col === 'age'){
+          this.workingRecord.age = this.entryForm.data;
+        }
+
+        return this.workingRecord;
+      }catch(err){
+        alert("Cannot add because "+err.message);
       }
     },
-    updateData(){},
-    removeData(){},
-    idExists(id){
-      return this.records.some(rec => rec.id === id);
+    validateColumn(column){
+      if(['name', 'age'].some(col => col === column)){
+        return column;
+      }else{
+        throw new Error(`column '${column}' does not exist`);
+      }
+    }
+    ,
+    validateID(id){
+      if(!this.records.some(rec => rec.id === id)){
+        return id;
+      }else{
+        throw new Error(`ID '${id}' already exists`);
+      };
     }
   }
 }).mount('#db-simulator');
