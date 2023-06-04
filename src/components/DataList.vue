486bf0f5f0d48487bb55273031f1648156b76e98
Luigi Moraes <luigimoraessantos@gmail.com>
2023-06-04 16:44:10 -0300

    DataList styled

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -5,7 +5,7 @@
         {{ col }}
       </th>
     </tr>
-    <tr v-for="rec in records.list" :key="rec.id">
+    <tr v-for="rec in recs.list" :key="rec.id">
       <td>{{ rec.id }}</td>
       <td>{{ rec.name }}</td>
       <td>{{ rec.age }}</td>
@@ -14,15 +14,35 @@
 </template>
 
 <script>
+import Record from "../classes/Record.js";
+
 export default {
   name: "DataList",
   data() {
-    return {
-      columns: [],
-      records: [],
-    };
+    return { columns: Record.getColumns() };
   },
+  props: ["recs"],
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+table {
+  font-family: "Inconsolata", "Courier New", Courier monospace;
+  width: 65vw;
+  margin-top: 0.75rem;
+  border: 1px solid black;
+  border-radius: 3px;
+
+  td,
+  th {
+    text-align: center;
+    padding: 0.25rem;
+    font-size: 0.9rem;
+  }
+
+  th {
+    border-bottom: 0.75px solid black;
+    font-size: 1.1rem;
+  }
+}
+</style>
