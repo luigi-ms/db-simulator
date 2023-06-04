307edb145ee1dac3862223affcca3609f16edca6
Luigi Moraes <luigimoraessantos@gmail.com>
2023-06-04 12:18:08 -0300

    started over

--- logic/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.esm-browser.prod.js"; 
-import InputForm from "./components/InputForm.js";
-import DataList from "./components/DataList.js";
-
-createApp(InputForm).mount("#input-form");
-createApp(DataList).mount("#data-list");
--- logic/stores.js
+++
@@ -1,7 +0,0 @@
-import Record from "./classes/Record.js";
-import RecordsMap from "./classes/RecordsMap.js";
-
-let actualRecord = new Record(0);
-const recordsMap = new RecordsMap();
-
-export { actualRecord, recordsMap };
